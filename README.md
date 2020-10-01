08abf2f51a4e91cbc957fb4423f890e16c7c03f8
nperalta-tw <nperalta@telware.com>
2020-10-01 23:36:06 +0000

    flask it

--- README.md
+++ README.md
@@ -3,3 +3,7 @@ My first website
 
 I'm exploring web-development at coding bootcamp with the Clubhou.se.  Our first assignment is create your first webpage
 with shayhowe.  This is my attempt at creating a website that introduces myself.
+
+
+- install requirements.txt `pip3 install -r requirements.txt`
+- start the webserver `python3 website.py`
---
+++ website.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    response = render_template('index.html')
+    return response
+
+@app.route("/interests")
+def interests():
+    response = render_template('interests.html')
+    return response
+
+@app.route("/aboutme")
+def aboutme():
+    response = render_template('aboutme.html')
+    return response
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8085, debug=True)
